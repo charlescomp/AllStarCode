60cee08f987fc77d420cfbd504371cc1a707a4e1
charlescomp <charles123430@gmail.com>
2016-07-15 15:47:22 -0400

    TESTING
    DJDBSINSK

---
+++ Conditionals.py
@@ -0,0 +1,10 @@
+from random import *
+
+houses = ["mansion","duplex","apartment","studio"]
+spouses=["Ashely","Jen","Sarah","Kate"]
+print (choice(houses))
+charles=raw_input("What house do you want to live in")
+houses.append(charles)
+print(choice(spouses))
+charles=raw_input("What is your wife's name")
+
---
+++ Random EX.py
@@ -0,0 +1,7 @@
+from random import *
+
+myList= ["apples","plum","oraganes", "mango","peach"]
+
+while True:
+    randomFruit= choice(myList)
+    print(randomFruit)
