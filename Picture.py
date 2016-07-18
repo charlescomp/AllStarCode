c39a88c171ba3446388a219bcabbe1519830b0c5
charlescomp <charles123430@gmail.com>
2016-07-18 10:36:00 -0400

    Picture
    OBAMAIFICATION

---
+++ Picture.py
@@ -0,0 +1,24 @@
+from Myro import *
+
+DarkBlue = makeColor(0,51,76)
+
+Red = makeColor(217, 26, 33)
+
+Blue = makeColor(112,150,158)
+
+Yellow = makeColor(252, 227, 166)
+
+
+
+pic = makePicture(pickAFile())
+pixels = getPixels (pic)
+for pixel in pixels:
+    if getGray(pixel)> 180:
+        setColor(pixel,Yellow)
+    elif getGray(pixel)> 120:
+        setColor(pixel,Blue)
+    elif getGray(pixel)> 60:
+        setColor(pixel,Red)
+show(pic)
+    
+    
